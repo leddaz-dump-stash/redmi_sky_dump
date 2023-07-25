2125e164d42fd93c580456f536d2c2977a6f2783
Leonardo Ledda <leonardoledda@gmail.com>
2023-07-25 21:01:24 +0000

    Add vendor for missi_phone_cn-user 13 TKQ1.221114.001 V14.0.4.0.TMWCNXM release-keys
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

---
+++ vendor/etc/opcust_exist.sh
@@ -0,0 +1,9 @@
+#！/bin/sh
+
+opcust=`ls /dev/block/by-name/opcust 2>/dev/null | tr -d "\r" ||true`
+
+if [ "$opcust" == "/dev/block/by-name/opcust" ];then
+    echo "exist"
+else
+    echo "non-exist"
+fi
